6dc863b7288d35fe468161d397419a88708f737f
IlyaErmolaev1606 <ioermolaev1606@gmail.com>
2023-12-21 00:41:26 +0300

    mUp1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Убедитесь, что этот файл существует и содержит стили для body, etc.
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Если вы хотите начать измерение производительности в вашем приложении, передайте функцию
+// для логирования результатов (например: reportWebVitals(console.log))
+// или отправьте в аналитический конечный пункт. Узнайте больше: https://bit.ly/CRA-vitals
+reportWebVitals();
